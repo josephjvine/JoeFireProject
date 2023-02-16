f3286789237a7e8e9dd7c90d6479106ff3c9d768
josephjvine <93606160+josephjvine@users.noreply.github.com>
2023-02-16 12:58:11 +0000

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAWT3MNAvhiqjW1R8uCraJWxqGJQBCyTbQ",
+    authDomain: "joefireproject.firebaseapp.com",
+    projectId: "joefireproject",
+    storageBucket: "joefireproject.appspot.com",
+    messagingSenderId: "624072404523",
+    appId: "1:624072404523:web:a0158dd7c6cf0e8b49446e",
+    measurementId: "G-NNPFCH6YL3"
+})
+
+var db = firebase.firestore()
